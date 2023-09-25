34ad97c70a132f55e485e0eb05c0f22bb07dc808
PekaEboy <pekaeboy@gmail.com>
2023-09-25 13:31:49 +0300

    Добавьте файлы проекта.

---
+++ RK1/RK1.cpp
@@ -0,0 +1,52 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <locale>
+// Вариант 5
+using namespace std;
+
+int main()
+{
+
+    setlocale(LC_ALL, "ru");
+    int n;
+    cout << "Введите число: ";
+    cin >> n;
+    int LEN = 1;
+    while (pow(10, LEN) <= n) {
+        LEN++;
+    }
+    if (LEN % 2 != 0) {
+        for (int i = 1; i <= LEN; i++) {
+
+            if (i % 2 == 0) {
+                cout << (n / (int)pow(10, LEN - i)) % 10;
+            }
+            else {
+                cout << (n / (int)pow(10, i - 1)) % 10;
+            }
+        }
+    }
+    else {
+        for (int i = 1; i <= LEN/2; i++) {
+
+            if (i % 2 == 0) {
+                cout << (n / (int)pow(10, LEN - i)) % 10;
+            }
+            else {
+                cout << (n / (int)pow(10, i - 1)) % 10;
+            }
+        }
+        for (int i = LEN / 2 +1; i <= LEN; i++) {
+
+            if (i % 2 == 0) {
+                cout << (n / (int)pow(10, i - 1)) % 10;
+            }
+            else {
+                cout << (n / (int)pow(10, LEN - i)) % 10;
+            }
+        }
+    }
+    cout << endl;
+    system("pause");
+    return 0;
+}
